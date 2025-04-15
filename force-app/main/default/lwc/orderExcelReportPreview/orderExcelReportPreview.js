153f063ecf71adb90dc0982856f1980a34848b36
Zsolt Harmath <harmathzs@gmail.com>
2025-04-15 15:49:35 +0200

    Add table preview and Excel file creation UI to orderExcelReportPreview component
    Implement a table preview with headers and data rows, along with functionality to create and download Excel files. Include a spinner for loading states and buttons for Create File, Download, and Close actions. Display the created file's ContentDocument link when available. Prepare handleCreateFile and handleShowTable methods for file operations, and handleCloseWindow for closing the preview.

--- force-app/main/default/lwc/orderExcelReportPreview/orderExcelReportPreview.js
+++ force-app/main/default/lwc/orderExcelReportPreview/orderExcelReportPreview.js
@@ -3,7 +3,74 @@
  */
 
 import {LightningElement, api} from 'lwc';
+import { loadScript } from 'lightning/platformResourceLoader';
+import { CloseActionScreenEvent } from 'lightning/actions';
+
+// import getTableData from '@salesforce/apex/AccountOrderReportController.getTableData';
+// import createContentVersion from '@salesforce/apex/AccountOrderReportController.createContentVersion';
 
 export default class OrderExcelReportPreview extends LightningElement {
 	@api recordId;
+
+	showSpinner = false;
+
+	headers = [];
+	data = [];
+
+	cdlId;
+	urlToCD;
+
+	async connectedCallback() {
+		this.showSpinner = true;
+
+		//await loadScript(this, sheetjs);
+
+		// Get the base64 encoded data
+		//const base64Data = await getTableData({accountId: this.recordId});
+		console.log('base64Data', base64Data);
+		// Decode base64 to string
+		// Use TextDecoder for UTF-8 decoding
+		const decodedString = new TextDecoder('utf-8').decode(
+			Uint8Array.from(atob(base64Data), c => c.charCodeAt(0))
+		);
+		console.log('decodedString', decodedString);
+		const jsonData = JSON.parse(decodedString);
+		console.log('jsonData', jsonData);
+		// First row is headers
+		this.headers = jsonData[0];
+		console.log('headers', this.headers);
+		// Rest is data
+		this.data = jsonData.slice(1).map((row, index) => ({
+			id: index,
+			cells: row
+		}));
+		console.log('data', this.data);
+
+		this.showSpinner = false;
+	}
+
+	async handleCreateFile(event) {
+		this.showSpinner = true;
+
+		//console.log('handleCreateFile event', event);
+		//this.cdlId = await createContentVersion({accountId: this.recordId});
+		this.urlToCD = '/'+this.cdlId;
+
+		this.showSpinner = false;
+	}
+
+	handleShowTable(event) {
+		//console.log('handleShowTable event', event);
+
+		// Create the URL for the Visualforce page with the record ID
+		let vfPageUrl = '/apex/AccountOrderReport?id=' + this.recordId;
+
+		// Open the URL in a new window/tab which will trigger the Excel download
+		window.open(vfPageUrl, '_blank');
+	}
+
+	handleCloseWindow(event) {
+		//console.log('handleCloseWindow event', event);
+		this.dispatchEvent(new CloseActionScreenEvent());
+	}
 }
