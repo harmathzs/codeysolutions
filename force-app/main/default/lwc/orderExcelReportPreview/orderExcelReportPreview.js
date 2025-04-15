29da9bc2cb7241961427777652880f9dca41c4c6
Zsolt Harmath <harmathzs@gmail.com>
2025-04-15 15:45:02 +0200

    Expose recordId as an API property and add component targets in metadata
    - Import the `api` decorator and expose the `recordId` property in the LWC
    - Add component targets and target configurations in the metadata file to enable usage on record pages and other contexts

--- force-app/main/default/lwc/orderExcelReportPreview/orderExcelReportPreview.js
+++ force-app/main/default/lwc/orderExcelReportPreview/orderExcelReportPreview.js
@@ -2,8 +2,8 @@
  * Created by Harmath Zsolt on 2025. 04. 15..
  */
 
-import {LightningElement} from 'lwc';
+import {LightningElement, api} from 'lwc';
 
 export default class OrderExcelReportPreview extends LightningElement {
-
+	@api recordId;
 }
