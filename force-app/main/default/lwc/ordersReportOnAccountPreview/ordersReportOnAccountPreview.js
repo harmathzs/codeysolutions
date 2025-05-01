7238d9414c7e884b765d23c3c9de94d7a7478916
Zsolt Harmath <harmathzs@gmail.com>
2025-05-01 14:56:37 +0200

    Introduce recordId parameter

--- force-app/main/default/lwc/ordersReportOnAccountPreview/ordersReportOnAccountPreview.js
+++ force-app/main/default/lwc/ordersReportOnAccountPreview/ordersReportOnAccountPreview.js
@@ -2,8 +2,8 @@
  * Created by Harmath Zsolt on 2025. 05. 01..
  */
 
-import {LightningElement} from 'lwc';
+import {LightningElement, api} from 'lwc';
 
 export default class OrdersReportOnAccountPreview extends LightningElement {
-
+	@api recordId;
 }
