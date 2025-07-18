fe6298298026162af39e064964ae6ead248f9548
Zsolt Harmath <harmathzs@gmail.com>
2025-07-18 13:36:56 +0200

    Show films in datatable

--- force-app/main/default/lwc/starWarsApiLwc/starWarsApiLwc.js
+++ force-app/main/default/lwc/starWarsApiLwc/starWarsApiLwc.js
@@ -4,12 +4,52 @@ export default class StarWarsApiLwc extends LightningElement {
 	films; people; planets; species; vehicles; starships;
 	filmsJSON; peopleJSON; planetsJSON; speciesJSON; vehiclesJSON; starshipsJSON;
 
+	filmsColumns = [
+		{ label: 'Title', fieldName: 'title' },
+		{ label: 'Created', fieldName: 'created', type: 'date' },
+		{ label: 'Director', fieldName: 'director' },
+		{ label: 'Edited', fieldName: 'edited', type: 'date' },
+		{ label: 'Episode ID', fieldName: 'episodeId' },
+		{ label: 'Opening Crawl', fieldName: 'openingCrawl' },
+		{ label: 'Producer', fieldName: 'producer' },
+		{ label: 'Release Date', fieldName: 'releaseDate', type: 'date' },
+		{ label: 'URL', fieldName: 'url', type: 'url' },
+	];
+	peopleColumns;
+	planetsColumns;
+	speciesColumns;
+	vehiclesColumns;
+	starshipsColumns;
+
+	filmsTableData;
+	peopleTableData;
+	planetsTableData;
+	speciesTableData;
+	vehiclesTableData;
+	starshipsTableData;
+
 	connectedCallback() {
 		fetch('https://swapi.info/api/films').then(res => res.json())
 			.then(data => {
 				this.films=data;
 				console.log('films', this.films);
 				this.filmsJSON = JSON.stringify(this.films);
+
+				// convert to table data
+				this.filmsTableData = this.films.map((film, idx) => {
+					return {
+						id: idx,
+						title: film.title,
+						created: film.created,
+						director: film.director,
+						edited: film.edited,
+						episodeId: film.episode_id,
+						openingCrawl: film.opening_crawl,
+						producer: film.producer,
+						releaseDate: film.release_date,
+						url: film.url,
+					}
+				});
 			})
 			.catch(console.warn);
 
