dc7039d62cffa66c8910069a917be65e3cef8441
Zsolt Harmath <harmathzs@gmail.com>
2025-07-18 14:02:21 +0200

    Show datatables

--- force-app/main/default/lwc/starWarsApiLwc/starWarsApiLwc.js
+++ force-app/main/default/lwc/starWarsApiLwc/starWarsApiLwc.js
@@ -27,10 +27,61 @@ export default class StarWarsApiLwc extends LightningElement {
 		{ label: 'Skin Color', fieldName: 'skin_color' },
 		{ label: 'URL', fieldName: 'url', type: 'url' },
 	];
-	planetsColumns;
-	speciesColumns;
-	vehiclesColumns;
-	starshipsColumns;
+	planetsColumns= [
+		{ label: 'Name', fieldName: 'name' },
+		{ label: 'Climate', fieldName: 'climate' },
+		{ label: 'Diameter', fieldName: 'diameter' },
+		{ label: 'Gravity', fieldName: 'gravity' },
+		{ label: 'Orbital Period', fieldName: 'orbital_period' },
+		{ label: 'Population', fieldName: 'population' },
+		{ label: 'Rotation Period', fieldName: 'rotation_period' },
+		{ label: 'Surface Water', fieldName: 'surface_water' },
+		{ label: 'Terrain', fieldName: 'terrain' },
+		{ label: 'URL', fieldName: 'url', type: 'url' },
+	];
+	speciesColumns= [
+		{ label: 'Name', fieldName: 'name' },
+		{ label: 'Average Height', fieldName: 'average_height' },
+		{ label: 'Average Lifespan', fieldName: 'average_lifespan' },
+		{ label: 'Classification', fieldName: 'classification' },
+		{ label: 'Designation', fieldName: 'designation' },
+		{ label: 'Eye Colors', fieldName: 'eye_colors' },
+		{ label: 'Hair Colors', fieldName: 'hair_colors' },
+		{ label: 'Homeworld', fieldName: 'homeworld', type: 'url' },
+		{ label: 'Language', fieldName: 'language' },
+		{ label: 'Skin Colors', fieldName: 'skin_colors' },
+		{ label: 'URL', fieldName: 'url', type: 'url' },
+	];
+	vehiclesColumns= [
+		{ label: 'Name', fieldName: 'name' },
+		{ label: 'Cargo Capacity', fieldName: 'cargo_capacity' },
+		{ label: 'Consumables', fieldName: 'consumables' },
+		{ label: 'Cost In Credits', fieldName: 'cost_in_credits' },
+		{ label: 'Crew', fieldName: 'crew' },
+		{ label: 'Length', fieldName: 'length' },
+		{ label: 'Manufacturer', fieldName: 'manufacturer' },
+		{ label: 'Max Atmosphering Speed', fieldName: 'max_atmosphering_speed' },
+		{ label: 'Model', fieldName: 'model' },
+		{ label: 'Passengers', fieldName: 'passengers' },
+		{ label: 'Vehicle Class', fieldName: 'vehicle_class' },
+		{ label: 'URL', fieldName: 'url', type: 'url' },
+	];
+	starshipsColumns= [
+		{ label: 'Name', fieldName: 'name' },
+		{ label: 'MGLT', fieldName: 'MGLT' },
+		{ label: 'cargo_capacity', fieldName: 'cargo_capacity' },
+		{ label: 'consumables', fieldName: 'consumables' },
+		{ label: 'cost_in_credits', fieldName: 'cost_in_credits' },
+		{ label: 'crew', fieldName: 'crew' },
+		{ label: 'hyperdrive_rating', fieldName: 'hyperdrive_rating' },
+		{ label: 'length', fieldName: 'length' },
+		{ label: 'manufacturer', fieldName: 'manufacturer' },
+		{ label: 'max_atmosphering_speed', fieldName: 'max_atmosphering_speed' },
+		{ label: 'model', fieldName: 'model' },
+		{ label: 'passengers', fieldName: 'passengers' },
+		{ label: 'starship_class', fieldName: 'starship_class' },
+		{ label: 'URL', fieldName: 'url', type: 'url' },
+	];
 
 	filmsTableData;
 	peopleTableData;
