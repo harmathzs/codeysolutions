d4d7eac963a04e9364101f44ce4dbc350f9063f9
Zsolt Harmath <harmathzs@gmail.com>
2025-07-18 14:51:26 +0200

    Add Bible.com Custom Metadata

--- force-app/main/default/lwc/bibleSearch/bibleSearch.js
+++ force-app/main/default/lwc/bibleSearch/bibleSearch.js
@@ -1,9 +1,15 @@
 /**
- * Created by User on 2025. 07. 18..
+ * Created by User on 2025. 07. 18.
  */
 
 import {LightningElement} from 'lwc';
+import getBibleComCustomSettings from '@salesforce/apex/BibleController.getBibleComCustomSettings';
 
 export default class BibleSearch extends LightningElement {
+	BibleComCustomSettings;
 
+	async connectedCallback() {
+		this.BibleComCustomSettings = await getBibleComCustomSettings();
+		console.log('BibleComCustomSettings', this.BibleComCustomSettings);
+	}
 }
