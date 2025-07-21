166850911f0310af81f1f59304ba6cd74fdbdc39
Zsolt Harmath <harmathzs@gmail.com>
2025-07-21 12:17:52 +0200

    Embed Visualforce in iframe

--- force-app/main/default/lwc/trainTrack/trainTrack.js
+++ force-app/main/default/lwc/trainTrack/trainTrack.js
@@ -26,11 +26,19 @@ export default class TrainTrack extends LightningElement {
 
 		const resObjStatus = await fetch('https://api.traintrack.hu/v1/status');
 		const resStatus = await resObjStatus.json();
-		console.log('resStatus', resStatus);
+		//console.log('resStatus', resStatus);
 		this.status = '';
 		for (const key in resStatus) {
 			this.status += `<p><strong>${key}</strong>: ${resStatus[key]}</p>`;
 		}
+
+		const resObjStats = await fetch('https://api.traintrack.hu/v1/stats');
+		const resStats = await resObjStats.json();
+		//console.log('resStats', resStats);
+		this.stats = '';
+		this.stats += `<p><strong>total_snapshots</strong>: ${resStats?.stats?.total_snapshots}</p>`;
+		this.stats += `<p><strong>avg_age_days</strong>: ${resStats?.stats?.avg_age_days}</p>`;
+
 
 		this.isLoading = false;
 	}
