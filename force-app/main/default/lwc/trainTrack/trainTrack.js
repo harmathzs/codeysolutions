4c1385e350a6a8ddf1fd48251f575f1e8f7f5783
Zsolt Harmath <harmathzs@gmail.com>
2025-07-21 12:08:51 +0200

    Prepare for embedding Visualforce in iframe

--- force-app/main/default/lwc/trainTrack/trainTrack.js
+++ force-app/main/default/lwc/trainTrack/trainTrack.js
@@ -32,62 +32,6 @@ export default class TrainTrack extends LightningElement {
 			this.status += `<p><strong>${key}</strong>: ${resStatus[key]}</p>`;
 		}
 
-		const resObjStats = await fetch('https://api.traintrack.hu/v1/stats');
-		const resStats = await resObjStats.json();
-		console.log('resStats', resStats);
-		this.stats = '';
-		this.stats += `<p><strong>total_snapshots</strong>: ${resStats?.stats?.total_snapshots}</p>`;
-		this.stats += `<p><strong>avg_age_days</strong>: ${resStats?.stats?.avg_age_days}</p>`;
-
-		const resObjTrains = await fetch('https://api.traintrack.hu/v1/trains');
-		const resTrains = await resObjTrains.json();
-		console.log('resTrains', resTrains);
-
-		if (resTrains.r2_url) {
-			// "https://bucket.traintrack.hu/trainstore/1752068756.json.gz"
-			//const resObjGzip = await fetch(resTrains.r2_url); // LWC MIME hiba! Helyette:
-			const blobStrGzipped = await fetchTrainGzip({url: resTrains.r2_url});
-			//console.log('Base64 GZ panel:', blobStrGzipped);
-
-			// => Base64 decode
-			const byteCharacters = atob(blobStrGzipped); // native browser fn
-			console.log('log1');
-
-			const byteNumbers = new Array(byteCharacters.length);
-			console.log('log2');
-			for (let i = 0; i < byteCharacters.length; i++) {
-				byteNumbers[i] = byteCharacters.charCodeAt(i);
-				console.log('logi');
-			}
-			console.log('loga');
-
-			const uint8Array = new Uint8Array(byteNumbers);
-			console.log('log3');
-
-			try {
-				this.fflate.gunzip(uint8Array, (err, decompressed) => {
-					if (err) {
-						console.warn('GUNZIP ERROR:', err);
-						//return;
-					}
-
-					const jsonText = new TextDecoder('utf-8').decode(decompressed);
-
-					try {
-						const data = JSON.parse(jsonText);
-						//console.log('✅ JSON parsed successfully:', data);
-						console.log('✅ JSON parsed successfully:');
-						// további feldolgozás
-
-					} catch (e) {
-						console.error('❌ JSON parse error:', e);
-					}
-				});
-			} catch (e) {
-				console.warn('Got error: ', e);
-			}
-		}
-
 		this.isLoading = false;
 	}
 }
