12527448ed484c627bc645153dbab053033ddacb
Zsolt Harmath <harmathzs@gmail.com>
2025-07-21 12:05:11 +0200

    Try to gunzip in the LWC

--- force-app/main/default/lwc/trainTrack/trainTrack.js
+++ force-app/main/default/lwc/trainTrack/trainTrack.js
@@ -1,5 +1,10 @@
 import {LightningElement} from 'lwc';
 
+import { loadScript } from 'lightning/platformResourceLoader';
+import FFLATE from '@salesforce/resourceUrl/fflate_lib'; // fflate.min.js static resource
+
+import fetchTrainGzip from '@salesforce/apex/TrainTrackController.fetchTrainGzip';
+
 export default class TrainTrack extends LightningElement {
 	status='TODO - status';
 	stats='TODO - stats';
@@ -7,8 +12,17 @@ export default class TrainTrack extends LightningElement {
 
 	isLoading = false;
 
+	fflate;
+	fflateLoaded = false;
+
 	async connectedCallback() {
 		this.isLoading = true;
+
+		if (!this.fflateLoaded) {
+			await loadScript(this, FFLATE);
+			this.fflate = window.fflate;
+			this.fflateLoaded = true;
+		}
 
 		const resObjStatus = await fetch('https://api.traintrack.hu/v1/status');
 		const resStatus = await resObjStatus.json();
@@ -28,6 +42,51 @@ export default class TrainTrack extends LightningElement {
 		const resObjTrains = await fetch('https://api.traintrack.hu/v1/trains');
 		const resTrains = await resObjTrains.json();
 		console.log('resTrains', resTrains);
+
+		if (resTrains.r2_url) {
+			// "https://bucket.traintrack.hu/trainstore/1752068756.json.gz"
+			//const resObjGzip = await fetch(resTrains.r2_url); // LWC MIME hiba! Helyette:
+			const blobStrGzipped = await fetchTrainGzip({url: resTrains.r2_url});
+			//console.log('Base64 GZ panel:', blobStrGzipped);
+
+			// => Base64 decode
+			const byteCharacters = atob(blobStrGzipped); // native browser fn
+			console.log('log1');
+
+			const byteNumbers = new Array(byteCharacters.length);
+			console.log('log2');
+			for (let i = 0; i < byteCharacters.length; i++) {
+				byteNumbers[i] = byteCharacters.charCodeAt(i);
+				console.log('logi');
+			}
+			console.log('loga');
+
+			const uint8Array = new Uint8Array(byteNumbers);
+			console.log('log3');
+
+			try {
+				this.fflate.gunzip(uint8Array, (err, decompressed) => {
+					if (err) {
+						console.warn('GUNZIP ERROR:', err);
+						//return;
+					}
+
+					const jsonText = new TextDecoder('utf-8').decode(decompressed);
+
+					try {
+						const data = JSON.parse(jsonText);
+						//console.log('✅ JSON parsed successfully:', data);
+						console.log('✅ JSON parsed successfully:');
+						// további feldolgozás
+
+					} catch (e) {
+						console.error('❌ JSON parse error:', e);
+					}
+				});
+			} catch (e) {
+				console.warn('Got error: ', e);
+			}
+		}
 
 		this.isLoading = false;
 	}
