ed01f34f458d8e0b90104d581e621fed18ca3db5
Zsolt Harmath <harmathzs@gmail.com>
2025-12-01 11:17:07 +0100

    Create LeadConverterLwc

---
+++ force-app/main/default/lwc/leadConverterLwc/leadConverterLwc.js
@@ -0,0 +1,10 @@
+/**
+ * Created by User on 2025. 12. 01..
+ */
+
+import {LightningElement, api} from 'lwc';
+
+export default class LeadConverterLwc extends LightningElement {
+	@api recordId;
+	@api ids;
+}
